601f6cf0b3a354de79d926d854f5137956c79c0e
AlanAruFlores <alanaruquipa242@gmail.com>
2024-06-03 15:40:15 -0300

    haciendo el juego v1.1

--- model/RespuestaModel.php
+++ model/RespuestaModel.php
@@ -7,7 +7,7 @@
         }
 
         public function getRespuestaByPreguntaId($preguntaId){
-            return $this->database->query("select r.id as res_id, r.esCorreto, r.descripción as descripcion_respuesta, p.id as preg_id, p.descripcion as descripcion_pregunta, p.punto, p.esValido from respuesta r join pregunta p on r.pregunta_id = p.id where p.id = $preguntaId;");
+            return $this->database->query("select r.id as res_id, r.esCorreto, r.descripción as descripcion_respuesta,r.letra, p.id as preg_id, p.descripcion as descripcion_pregunta, p.punto, p.esValido from respuesta r join pregunta p on r.pregunta_id = p.id where p.id = $preguntaId;");
         }
 
         //Metodo para crear un nuevo Usuario
--- quizquest_db.sql
+++ quizquest_db.sql
@@ -93,6 +93,7 @@ CREATE TABLE Respuesta
     id          INT PRIMARY KEY,
     esCorreto   BOOLEAN NOT NULL,
     descripción TEXT    NOT NULL,
+    letra varchar(10) NOT NULL,
     pregunta_id INT,
     FOREIGN KEY (pregunta_id) REFERENCES Pregunta (id)
 );
@@ -167,74 +168,75 @@ VALUES (1, 1),
 */
 -- Insertar datos en la tabla Respuesta para cada pregunta
 -- Pregunta 1
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (1, TRUE, 'París', 1),
-       (2, FALSE, 'Londres', 1),
-       (3, FALSE, 'Roma', 1),
-       (4, FALSE, 'Berlín', 1);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (1, TRUE, 'París', 'A', 1),
+       (2, FALSE, 'Londres', 'B', 1),
+       (3, FALSE, 'Roma', 'C', 1),
+       (4, FALSE, 'Berlín', 'D', 1);
 
 -- Pregunta 2
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (5, FALSE, 'Newton', 2),
-       (6, TRUE, 'Einstein', 2),
-       (7, FALSE, 'Galileo', 2),
-       (8, FALSE, 'Tesla', 2);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (5, FALSE, 'Newton', 'A', 2),
+       (6, TRUE, 'Einstein', 'B', 2),
+       (7, FALSE, 'Galileo', 'C', 2),
+       (8, FALSE, 'Tesla', 'D', 2);
 
 -- Pregunta 3
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (9, FALSE, 'Amazonas', 3),
-       (10, FALSE, 'Nilo', 3),
-       (11, TRUE, 'Yangtsé', 3),
-       (12, FALSE, 'Mississippi', 3);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (9, FALSE, 'Amazonas', 'A', 3),
+       (10, FALSE, 'Nilo', 'B', 3),
+       (11, TRUE, 'Yangtsé', 'C', 3),
+       (12, FALSE, 'Mississippi', 'D', 3);
 
 -- Pregunta 4
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (13, FALSE, '2001', 4),
-       (14, TRUE, '1991', 4),
-       (15, FALSE, '1981', 4),
-       (16, FALSE, '2011', 4);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (13, FALSE, '2001', 'A', 4),
+       (14, TRUE, '1991', 'B', 4),
+       (15, FALSE, '1981', 'C', 4),
+       (16, FALSE, '2011', 'D', 4);
 
 -- Pregunta 5
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (17, TRUE, 'Miguel de Cervantes', 5),
-       (18, FALSE, 'Gabriel García Márquez', 5),
-       (19, FALSE, 'Mario Vargas Llosa', 5),
-       (20, FALSE, 'Pablo Neruda', 5);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (17, TRUE, 'Miguel de Cervantes', 'A', 5),
+       (18, FALSE, 'Gabriel García Márquez', 'B', 5),
+       (19, FALSE, 'Mario Vargas Llosa', 'C', 5),
+       (20, FALSE, 'Pablo Neruda', 'D', 5);
 
 -- Pregunta 6
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (21, FALSE, '3', 6),
-       (22, TRUE, '7', 6),
-       (23, FALSE, '5', 6),
-       (24, FALSE, '4', 6);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (21, FALSE, '3', 'A', 6),
+       (22, TRUE, '7', 'B', 6),
+       (23, FALSE, '5', 'C', 6),
+       (24, FALSE, '4', 'D', 6);
 
 -- Pregunta 7
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (25, FALSE, 'O2', 7),
-       (26, FALSE, 'CO2', 7),
-       (27, TRUE, 'H2O', 7),
-       (28, FALSE, 'N2', 7);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (25, FALSE, 'O2', 'A', 7),
+       (26, FALSE, 'CO2', 'B', 7),
+       (27, TRUE, 'H2O', 'C', 7),
+       (28, FALSE, 'N2', 'D', 7);
 
 -- Pregunta 8
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (29, FALSE, '20', 8),
-       (30, TRUE, '24', 8),
-       (31, FALSE, '30', 8),
-       (32, FALSE, '28', 8);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (29, FALSE, '20', 'A', 8),
+       (30, TRUE, '24', 'B', 8),
+       (31, FALSE, '30', 'C', 8),
+       (32, FALSE, '28', 'D', 8);
 
 -- Pregunta 9
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (33, TRUE, 'George Washington', 9),
-       (34, FALSE, 'Thomas Jefferson', 9),
-       (35, FALSE, 'Abraham Lincoln', 9),
-       (36, FALSE, 'John Adams', 9);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (33, TRUE, 'George Washington', 'A', 9),
+       (34, FALSE, 'Thomas Jefferson', 'B', 9),
+       (35, FALSE, 'Abraham Lincoln', 'C', 9),
+       (36, FALSE, 'John Adams', 'D', 9);
 
 -- Pregunta 10
-INSERT INTO Respuesta (id, esCorreto, descripción, pregunta_id)
-VALUES (37, FALSE, 'Mercurio', 10),
-       (38, TRUE, 'Venus', 10),
-       (39, FALSE, 'Marte', 10),
-       (40, FALSE, 'Júpiter', 10);
+INSERT INTO Respuesta (id, esCorreto, descripción, letra, pregunta_id)
+VALUES (37, FALSE, 'Mercurio', 'A', 10),
+       (38, TRUE, 'Venus', 'B', 10),
+       (39, FALSE, 'Marte', 'C', 10),
+       (40, FALSE, 'Júpiter', 'D', 10);
+
        
 /*SELECT*/
 select p.id, p.descripcion, p.punto, c.nombre as categoria, c.color as color from pregunta p join categoria c on p.categoría_id = c.id order by rand() limit 1;
