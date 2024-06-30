6273c41169eef270b444946ea26e734715c8c4e0
AlanAruFlores <alanaruquipa242@gmail.com>
2024-06-30 16:45:50 -0300

    reloj

--- public/js/ajax/temporizadorAjax.js
+++ public/js/ajax/temporizadorAjax.js
@@ -2,18 +2,18 @@ const $contadorTiempoElem = document.querySelector("#contador_tiempo");
 
 
 //Temporizador
-// setInterval(()=>{
-//     $.ajax({
-//         url:"/quizquest/contar/get",
-//         type:"GET",
-//         datatype: 'json',
-//         success: function($temporizador){
-//             $contadorTiempoElem.innerHTML = $temporizador.segundos;
-//             if($temporizador.segundos == 0)
-//                 location.href="http://localhost/quizquest/juego/goToTheEnd";
-//         }
-//     });
-// },1000);
+setInterval(()=>{
+    $.ajax({
+        url:"/quizquest/contar/get",
+        type:"GET",
+        datatype: 'json',
+        success: function($temporizador){
+            $contadorTiempoElem.innerHTML = $temporizador.segundos;
+            if($temporizador.segundos == 0)
+                location.href="http://localhost/quizquest/juego/goToTheEnd";
+        }
+    });
+},1000);
 
 
 
